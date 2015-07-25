4dc3ec9334c1caa4854848b49a294b1ab36611f0
= <=>
2015-07-25 15:08:39 -0600

    Created level 8.  Created button switch. -Adam

---
+++ graphics/items/Button.js
@@ -0,0 +1,14 @@
+﻿(function(window) {
+mcButtonSwitch = function() {
+	this.initialize();
+}
+mcButtonSwitch._SpriteSheet = new createjs.SpriteSheet({images: ["Button.png"], frames: [[0,0,156,49,0,0,0],[0,0,156,49,0,0,0],[0,0,156,49,0,0,0],[0,0,156,49,0,0,0],[0,0,156,49,0,0,0],[0,0,156,49,0,0,0],[0,0,156,49,0,0,0],[0,0,156,49,0,0,0],[0,0,156,49,0,0,0],[0,49,156,41,0,0,-8]]});
+var mcButtonSwitch_p = mcButtonSwitch.prototype = new createjs.Sprite();
+mcButtonSwitch_p.Sprite_initialize = mcButtonSwitch_p.initialize;
+mcButtonSwitch_p.initialize = function() {
+	this.Sprite_initialize(mcButtonSwitch._SpriteSheet);
+	this.paused = false;
+}
+window.mcButtonSwitch = mcButtonSwitch;
+}(window));
+
